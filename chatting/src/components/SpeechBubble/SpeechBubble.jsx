9d58d9be68ab5f08d8a2a2a30706015c417fbb42
jio <pennicillium@daum.net>
2024-02-08 23:00:13 +0900

    Feat : 말풍선 컴포넌트 스타일링

--- chatting/src/components/SpeechBubble/SpeechBubble.jsx
+++ chatting/src/components/SpeechBubble/SpeechBubble.jsx
@@ -14,7 +14,14 @@ function SpeechBubble() {
             Can we meet tomorrow ?I am doing well, Can we meet tomorrow ?
           </span>
         </div>
-        <div className={`${style.circle} ${style.other_circle}`}></div>
+        <div className={style.bubble_info}>
+          <div
+            role="none"
+            className={`${style.circle} ${style.other_circle}`}
+          ></div>
+          {/*  위 메시지와 시간이 같으면 생략 */}
+          <span>Today, 8:30 pm</span>
+        </div>
       </div>
       {/* 내 말풍선 */}
       <div className={style.speech_bubble_wrapper}>
@@ -25,7 +32,14 @@ function SpeechBubble() {
             Can we meet tomorrow ?I am doing well, Can we meet tomorrow ?
           </span>
         </div>
-        <div className={`${style.circle} ${style.my_circle}`}></div>
+        <div className={`${style.bubble_info} ${style.my_info}`}>
+          {/*  위 메시지와 시간이 같으면 생략 */}
+          <span className={style.info}>Today, 8:30 pm</span>
+          <div
+            role="none"
+            className={`${style.circle} ${style.my_circle}`}
+          ></div>
+        </div>
       </div>
     </>
   );
