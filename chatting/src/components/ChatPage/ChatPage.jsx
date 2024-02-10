f1f622ebf82fa5842cfe23d24be8da7f907b6641
jio <pennicillium@daum.net>
2024-02-10 12:27:44 +0900

    Feat : NavBar 렌더링

--- chatting/src/components/ChatPage/ChatPage.jsx
+++ chatting/src/components/ChatPage/ChatPage.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import { Chatroom, NavBar, ChatroomList } from "../index";
 import style from "./ChatPage.module.css";
 // import { string } from "prop-types";
+const user_id = "450foql2mb3cx6s";
+const API_USERS_ENDPOINT = `${
+  import.meta.env.VITE_PB_API
+}api/collections/users/records?filter=(id="${user_id}")`;
+
 function ChatPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(API_USERS_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => setData(data.items[0]))
+      .catch((Error) => console.log(Error));
+  }, []);
+
   return (
     <div className={style.ChatPage}>
-      <NavBar />
-      <ChatroomList />
+      <NavBar userData={data} />
+      <ChatroomList userId={data.id} />
       <Chatroom />
     </div>
   );
