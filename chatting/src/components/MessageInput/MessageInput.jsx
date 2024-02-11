57e74016d9a276ff92e4d1a23b65119ddbd8ec0c
jio <pennicillium@daum.net>
2024-02-11 20:50:44 +0900

    Feat : 메시지 전송 기능

--- chatting/src/components/MessageInput/MessageInput.jsx
+++ chatting/src/components/MessageInput/MessageInput.jsx
@@ -1,20 +1,43 @@
 import style from "./Message.module.css";
 // import { string } from "prop-types";
+import { useRef } from "react";
+import { timeStamp } from "../../utils";
 
-/*
-1. 디바운싱 적용 
-2. 1자 이상의 메시지를 입력했을때 전송이 되어야됨 => 상응하는 애니메이션도 ㄱㅊ 
-*/
-function MessageInput() {
+import PocketBase from "pocketbase";
+const pb = new PocketBase(import.meta.env.VITE_PB_API);
+const TEST_ID = "450foql2mb3cx6s";
+
+function MessageInput({ chatRoomIdx }) {
+  const message = useRef("");
+
+  // 전송버튼 클릭/엔터 누르면 작동하는 이벤트 함수
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const messageContent = message.current.value;
+    console.log(messageContent);
+    console.log(Boolean(messageContent));
+    if (messageContent) {
+      const now = Date.now();
+      const message_data = {
+        chatroom_idx: chatRoomIdx,
+        sender: TEST_ID,
+        message_content: messageContent,
+        sent_at: timeStamp(now),
+      };
+      await pb.collection("message").create(message_data);
+    }
+  }
+
   return (
-    <form className={style.message_form}>
+    <form onSubmit={handleSubmit} className={style.message_form}>
       <input
         className={style.message_input}
         type="text"
         placeholder="Type your message here...😉"
+        ref={message}
       />
-      <button className={style.message_send} type="button">
-        <img src="../../../public/send.svg" alt="" />
+      <button className={style.message_send} type="submit">
+        <img src="/send.svg" alt="" />
       </button>
     </form>
   );
