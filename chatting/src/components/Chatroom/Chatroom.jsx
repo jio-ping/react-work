fb89d870f6ad17577a27869658a373b5449d5fd5
jio <pennicillium@daum.net>
2024-02-11 17:01:41 +0900

    Feat : 채팅기록 보기 기능

--- chatting/src/components/Chatroom/Chatroom.jsx
+++ chatting/src/components/Chatroom/Chatroom.jsx
@@ -4,14 +4,43 @@ import {
   MessageInput,
   ChatroomInfo,
 } from "/src/components/index.js";
+import { useEffect, useState } from "react";
+import PocketBase from "pocketbase";
+const pb = new PocketBase(import.meta.env.VITE_PB_API);
+
 // import { string } from "prop-types";
+const API_MESSAGE_ENDPOINT = `${
+  import.meta.env.VITE_PB_API
+}api/collections/message/records`;
+// 선택한 채팅방
 function Chatroom() {
+  // 선택한 채팅방
+  const [chatRoom, setChatroom] = useState("1");
+
+  const [message, setMessage] = useState([]);
+  // realtime API
+  useEffect(() => {
+    pb.collection("message").subscribe("*", function (e) {
+      setMessage(e.record);
+    });
+  }, []);
+
+  // 채팅창
+  useEffect(() => {
+    const 선택한_채팅방_메시지 = `${API_MESSAGE_ENDPOINT}?sort=sent_at,sender&filter=(chatroom_idx="${chatRoom}")`;
+    fetch(선택한_채팅방_메시지)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.items))
+      .catch((Error) => console.error(Error));
+  }, []);
+
   return (
     <div className={style.chatroom}>
       <ChatroomInfo />
       <div className={style.Bubbles}>
-        <SpeechBubble sender="너" />
-        <SpeechBubble sender="나" />
+        {message.map((item) => (
+          <SpeechBubble key={item.id} messageInfo={item} />
+        ))}
       </div>
       <MessageInput />
     </div>
